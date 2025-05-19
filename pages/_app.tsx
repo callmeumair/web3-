e4cda5631423f76c1fd4a572cfcd2b9df0de1af9
Umair <umerpatel1540@gmail.com>
2025-05-20 02:34:55 +0530

    feat: add favicon support and wrap application in Head component for improved SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider, createConfig, http } from 'wagmi';
 import { hardhat } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@rainbow-me/rainbowkit/styles.css';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 const config = createConfig({
   chains: [hardhat],
@@ -16,13 +18,18 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
   );
 }
 
